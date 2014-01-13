5613919096390cde9591f21622a7185ed86abaf6
Sandro Milhano <sandro.milhano@hotmail.com>
2014-01-13 18:24:35 +0000

    First commit

---
+++ src/bg/background.js
@@ -0,0 +1,29 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+var menu = chrome.contextMenus.create({
+    "title": "Archive This!",
+    "contexts": ["all"]
+  });
+
+chrome.contextMenus.onClicked.addListener(function(activeTab)
+{
+
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    var siteUrl = tabs[0].url;
+	var arquiveUrl = "http://web.archive.org/save/";
+	
+    arquiveUrl += encodeURI(siteUrl);
+
+    // Open the page up.
+	chrome.tabs.create(
+		{
+			"url" : arquiveUrl
+		}
+	);
+});
+
+});
